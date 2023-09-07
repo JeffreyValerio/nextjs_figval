3b63be8e47789242a572daa9ff43c70406f2cd64
JeffreyValerio <jeffreyvalerio@hotmail.com>
2023-09-06 22:39:05 -0600

    layout

--- src/app/layouts/ContentLayout.tsx
+++ src/app/layouts/ContentLayout.tsx
@@ -4,7 +4,7 @@ interface Props {
 }
 const ContentLayout = ({ children, className }: Props) => {
     return (
-        <div className={`${className || ''} max-w-7xl mx-auto py-2 md:py-4`}>
+        <div className={`${className || ''} max-w-7xl mx-auto py-2 md:py-4 px-2 md:px-0`}>
             {children}
         </div>
     )
