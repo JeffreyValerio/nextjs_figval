b43f18f2b45a7977ddcb03e985d7929b3b302ed5
JeffreyValerio <jeffreyvalerio@hotmail.com>
2023-09-08 08:28:08 -0600

    zoom images

--- src/components/sections/ProductCarousel.tsx
+++ src/components/sections/ProductCarousel.tsx
@@ -2,6 +2,7 @@
 
 import { Carousel } from 'react-responsive-carousel';
 import "react-responsive-carousel/lib/styles/carousel.min.css";
+import ZoomImage from '../shared/ZoomImage';
 
 interface Props {
     images: {
@@ -21,12 +22,14 @@ const ProductCarousel = ({ images }: Props) => {
             >
                 {images.data?.map((image: any) => (
                     // eslint-disable-next-line @next/next/no-img-element
-                    <img
-                        className="object-scale-down bg-[#f2f2f2]"
-                        key={image.id}
-                        src={`${image.attributes.url || '/images/no-image.png'}`}
-                        alt={image.attributes.name}
-                    />
+                    <>
+                        <ZoomImage key={image.id}  src={`${image.attributes.url || '/images/no-image.png'}`} />
+                        <img
+                            className="object-scale-down bg-[#f2f2f2]"
+                            key={image.id} 
+                            src={`${image.attributes.url || '/images/no-image.png'}`}
+                            alt={image.attributes.name} />
+                    </>
                 ))}
             </Carousel>
         </div>
---
+++ src/components/shared/ZoomImage.tsx
@@ -0,0 +1,56 @@
+'use client'
+
+import { useState } from 'react';
+
+function ZoomImage({ key, src }: any) {
+    const [zoomStyle, setZoomStyle] = useState({
+        transform: 'scale(1) translate(0, 0)',
+    });
+
+    function handleMouseMove(e: any) {
+        const img = e.currentTarget;
+        const boundingBox = img.getBoundingClientRect();
+        const offsetX = e.clientX - boundingBox.left;
+        const offsetY = e.clientY - boundingBox.top;
+
+        const scaleX = 2; // Ajusta el nivel de zoom
+        const scaleY = 2; // Ajusta el nivel de zoom
+
+        const transform = `scale(${scaleX}, ${scaleY}) translate(${-offsetX}px, ${-offsetY}px)`;
+
+        setZoomStyle({ transform });
+    }
+
+    function handleMouseOut() {
+        setZoomStyle({ transform: 'scale(1) translate(0, 0)' });
+    }
+
+    return (
+        <div
+            className="zoom-image"
+            onMouseMove={handleMouseMove}
+            onMouseOut={handleMouseOut}
+        >
+            <img className='block' key={key} src={src} alt="Imagen" style={zoomStyle} />
+            <style jsx>{`
+        .zoom-image {
+          position: relative;
+          width: 100%; /* Ajusta el ancho y alto según tus necesidades */
+          height: auto;
+          overflow: hidden;
+          background: #f2f2f2;
+          padding: 5px;
+        }
+
+        .zoom-image img {
+          width: 100%;
+          height: auto;
+          transition: transform 0.1s ease;
+          transform-origin: 0 0;
+        }
+      `}</style>
+        </div>
+    );
+}
+
+export default ZoomImage;
